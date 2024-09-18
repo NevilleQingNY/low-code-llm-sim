1143248ccaaf67c8c4cc2ccfe78111314504ab7b
NevilleQingNY <nevilleqing@gmail.com>
2024-09-18 21:56:16 +0800

    fix：修复tabs问题

--- components/CustomOutputNode.tsx
+++ components/CustomOutputNode.tsx
@@ -256,7 +256,7 @@ export const CustomOutputNode: React.FC<CustomOutputNodeProps> = ({ id, data, is
                                                         value={node.id}
                                                         className="inline-flex items-center justify-center whitespace-nowrap px-3 py-1.5 text-sm font-medium ring-offset-background transition-all focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-primary data-[state=active]:text-primary-foreground data-[state=active]:shadow-sm"
                                                     >
-                                                        {(node.data?.name as string) ?? '未命名'}
+                                                        {(node.data?.name as string) || '未命名'}
                                                     </TabsTrigger>
                                                 ))}
                                             </TabsList>
